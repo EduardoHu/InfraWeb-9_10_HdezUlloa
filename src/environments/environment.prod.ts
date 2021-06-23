a1d9177670b830a14921fbda4e74c1d31b2463f8
EduardoHu <joedhernandezul@ittepic.edu.mx>
2021-06-22 18:21:12 -0600

    Produccion agregada

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyACf0LBK_ng5ixc5SNRmiOl2uRrJje_cQ8",
+    authDomain: "restaurant-d0ecd.firebaseapp.com",
+    databaseURL: "https://restaurant-d0ecd-default-rtdb.firebaseio.com",
+    projectId: "restaurant-d0ecd",
+    storageBucket: "restaurant-d0ecd.appspot.com",
+    messagingSenderId: "1030303441762",
+    appId: "1:1030303441762:web:32e336e6ae9d1adc32423b"
+    
+  }
+};
